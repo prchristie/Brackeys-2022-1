419ac9c79548e318a08c293c3bb68da3efcc58cc
Your Name <you@example.com>
2022-02-22 16:21:42 +1030

    First commit

---
+++ Assets/Node.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Vector2 Position { get; private set; }
+    public List<Node> Edges { get; private set; }
+    
+    
+}
